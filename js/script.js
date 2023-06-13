d6e6f032c5165b16282f831576bd03f75358b871
Daniel Svindseth <daniel@svindseth.net>
2023-06-14 01:32:15 +0200

    Update to v3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //
 // INSTRUKSJONAR
 // Bytt ut tala nedanfor med kor mange bilder som ligg totalt i dei tilsvarande mappene
-//
+// ENDRE IKKJE PÅ LINJENR FOR DESSE VARIABLANE! SKAL VERE 8 TIL 13
 // Når du legg til eit nytt bilde, følger du det eksisterande namnemønsteret t.d. dyr057 for det 57ande bildet i kategorien dyr.
 //
 // SKRIV TALET PÅ BILDER I KVAR KATEGORI
@@ -20,12 +20,12 @@ observer.observe();
 
 function activeButtons() {
   //Make buttons active
-  var header = document.getElementById("nav-top-buttons");
-  var btns = header.getElementsByClassName("header-button");
+  // var header = document.getElementById("nav-top-buttons");
+  var btns = document.getElementById("nav-top-buttons").getElementsByClassName("header-button");
   for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active-header-button");
-    current[0].className = current[0].className.replace(" active-header-button", "");
+    // var current = document.getElementsByClassName("active-header-button");
+    document.getElementsByClassName("active-header-button")[0].className = document.getElementsByClassName("active-header-button")[0].className.replace(" active-header-button", "");
     this.className += " active-header-button";
     });
   }
@@ -33,33 +33,30 @@ function activeButtons() {
 
 //Split the images into 4 columns
 function chooseColumn() {
-  var columnNr = 1;
-  var column = getElementById("column" + columnNr);
-  if (columnNr < 4){
-    columnNr++;
-  } else {
-    columnNr = 1;
-  }
-  return columnNr;
+  console.error('chooseColumn is deprecated');
+  // var columnNr = 1;
+  // var column = getElementById("column" + columnNr);
+  // if (columnNr < 4){ columnNr++; } else { columnNr = 1; }
+  // return columnNr;
 }
-var nr = 01;
+var nr = 001;
 
 function removeImages() {
-  const elem1 = document.getElementById("column1");
-  elem1.innerHTML = '';
-  const elem2 = document.getElementById("column2");
-  elem2.innerHTML = '';
-  const elem3 = document.getElementById("column3");
-  elem3.innerHTML = '';
-  const elem4 = document.getElementById("column4");
-  elem4.innerHTML = '';
+  document.getElementById("photos-view").innerHTML = '';
+  // document.getElementById("column1").innerHTML = '';
+  // document.getElementById("column2").innerHTML = '';
+  // document.getElementById("column3").innerHTML = '';
+  // document.getElementById("column4").innerHTML = '';
 }
 
-function loadImages(category, categoryNr){
+localStorage.setItem('category_slice', 1);
+
+function loadImages(category, categoryNr, isAll){
+  clearCollections();
   console.log(category);
-  var i = 0;
-  var u;
-  var c = 1;
+  var i = 0; var u; var c;
+  if (isAll == true){ c = localStorage.getItem('category_slice'); } else { c = 1; }
+
   nr = 1;
   //let categoryNr = category + 'Nr';
   console.log(categoryNr);
@@ -75,44 +72,34 @@ function loadImages(category, categoryNr){
     photo.setAttribute("class", "photo");
     photo.setAttribute("id", "img" + category + u);
     photo.setAttribute("loading", "lazy");
-    document.getElementById("column" + c).appendChild(box);
+    document.getElementById('photos-view').appendChild(box);
+    // document.getElementById("column" + c).appendChild(box);
     document.getElementById("box" + category + u).appendChild(photo);
     photo.setAttribute("onclick", "imageViewer(src);");
-    nr++;
-    i++;
-    if (c < 4){
-      c++;
-    }
-    else {
-      c = 1;
-    }
+    nr++; i++;
+
+    // Split into 4 columns
+  //   if (c < 4){ c++; } else { c = 1; }
+  //   if (i == categoryNr - 1) {
+  //     console.log(categoryNr);
+  //   }
+    localStorage.setItem('category_slice', c);
   }
 }
 
-function show(category, categoryNr) {
+function show(category, categoryNr, isAll) {
   removeImages();
-  loadImages(category, categoryNr);
+  loadImages(category, categoryNr, isAll);
 }
 
 function showAll() {
   removeImages();
-  loadImages('dyr', DyrNr);
-  loadImages('por', PorNr);
-  loadImages('lan', LanNr);
-  loadImages('pla', PlaNr);
-  loadImages('div', DivNr);
+  loadImages('dyr', DyrNr, true);
+  loadImages('por', PorNr, true);
+  loadImages('lan', LanNr, true);
+  loadImages('pla', PlaNr, true);
+  loadImages('div', DivNr, true);
 }
-
-/*
-function clickImage() {
-  getElementById();
-}
-*/
-
-function homePage() {
-
-}
-
 
 function imageViewer(inputImage) {
   document.addEventListener("keyup", keyPress);
@@ -165,21 +152,44 @@ function closeViewer() {
   document.body.focus();
   body.removeAttribute("class", "no-scroll");
   document.removeEventListener("keyup", keyPress);
+}
 
+function analyzeNextSrc(src, nr, direction) {
+  nowNr = pad(nr);
+  if (direction == 'next') {
+    nextNr = pad(nr + 1);
+  }
+  else if (direction == 'prev') {
+    nextNr = pad(nr - 1);
+  }
+  var nextSrc = src.replace(nowNr, nextNr);
+  var img = new Image();
+  img.src = nextSrc;
+  console.log("nextSrc = " + nextSrc);
+  return img.height != 0;
 }
 
 function nextImage() {
   var imageViewChild = document.getElementById("image-viewer-child");
   var oldSrc = imageViewChild.getAttribute("src");
   var oldNr = oldSrc.slice(-7 ,-4);
-  console.log(oldNr);
+  console.log('oldNr = ' + oldNr);
   var oldGrp = oldSrc.slice(-10, -7);
-  console.log(oldGrp);
+  console.log('oldGrp = ' + oldGrp);
   var newNr = Number(oldNr);
-  console.log(newNr);
+  console.log('newNr = ' + newNr);
   var newGrp = oldGrp;
-  console.log(newGrp);
-  if (localStorage.getItem('currentCategory') == 'all') {
+  console.log('newGrp = ' + newGrp);
+  if (localStorage.getItem('currentCategory') == 'col') {
+    // if (analyzeNextSrc(oldSrc, newNr) == true) {
+    if (analyzeNextSrc(oldSrc, newNr, 'next') == false) {
+      newNr = Number(oldNr);
+    }
+    else {
+      newNr++;
+    }
+  }
+  else if (localStorage.getItem('currentCategory') == 'all') {
     if (oldGrp == "dyr" && newNr >= DyrNr) {
       newNr = 1;
       newGrp = 'por';
@@ -242,7 +252,16 @@ function prevImage() {
   var oldGrp = oldSrc.slice(-10, -7);
   var newNr = Number(oldNr);
   var newGrp = oldGrp;
-  if (localStorage.getItem('currentCategory') == 'all') {
+  if (localStorage.getItem('currentCategory') == 'col') {
+    // if (analyzeNextSrc(oldSrc, newNr) == true) {
+    if (analyzeNextSrc(oldSrc, newNr, 'prev') == false) {
+      newNr = 1;
+    }
+    else {
+      newNr--;
+    }
+  }
+  else if (localStorage.getItem('currentCategory') == 'all') {
     if (newNr <= 1) {
       if (oldGrp == "dyr") {
         newNr = DivNr;
@@ -306,7 +325,6 @@ function loopAround() {
   var oldSrc = imageViewChild.getAttribute("src");
   var grp = oldSrc.slice(-9, -6);
   if (localStorage.getItem('currentCategory') == 'all') {
-    console.log('1');
   }
 }
 
@@ -318,11 +336,16 @@ function resetCurrentCategory() {
   localStorage.setItem('currentCategory', 'all');
 }
 
+// add 0 or 00 to numbers to make a 3 char ing length string. Does not work for numbers over 999
 function pad(d) {
-  if (d < 100 && d > 9) {
+  if (d < 100 && d > 9) { // numbers between 9 and 100
     return (d < 100) ? '0' + d.toString() : toString();
-  } else {
+  }
+  else if (d < 10) { // numbers under 10
     return (d < 10) ? '00' + d.toString() : toString();
+  }
+  else { // numbers over 99
+    return d.toString();
   }
 }
 
@@ -335,27 +358,20 @@ function keyPress() {
     closeViewer();
   }
 }
-
-function hideSidebar() {
-  var sidebar = document.getElementById("sidebar");
-  sidebar.style.display = "none";
-}
-
-function showSidebar() {
-  var sidebar = document.getElementById("sidebar");
-  sidebar.removeAttribute("class", "hidden-phone");
-}
-
-function toggleSidebar() {
-  var sidebar = document.getElementById("sidebar");
-  if (sidebar.getAttribute("class") === "phone-hidden") {
-    sidebar.removeAttribute("class", "phone-hidden");
-    body.setAttribute("class", "no-scroll");
-  } else {
-    sidebar.setAttribute("class", "phone-hidden");
-    body.removeAttribute("class", "no-scroll");
-  }
-}
+//
+// function hideSidebar() { document.getElementById("sidebar").style.display = "none"; }
+// function showSidebar() { document.getElementById("sidebar").removeAttribute("class", "hidden-phone"); }
+//
+// function toggleSidebar() {
+//   var sidebar = document.getElementById("sidebar");
+//   if (sidebar.getAttribute("class") === "phone-hidden") {
+//     sidebar.removeAttribute("class", "phone-hidden");
+//     body.setAttribute("class", "no-scroll");
+//   } else {
+//     sidebar.setAttribute("class", "phone-hidden");
+//     body.removeAttribute("class", "no-scroll");
+//   }
+// }
 
 function toggleMenu() {
   var menu = document.getElementById("nav-popup-menu");
@@ -369,6 +385,7 @@ function toggleMenu() {
   }
 }
 
+// HTML drop-down menu for changing category
 function categoryChange() {
   const category_switcher = document.getElementById("category");
   let cat = category_switcher.options.selectedIndex;
@@ -378,44 +395,89 @@ function categoryChange() {
       setCurrentCategory('all');
       oki();
       break;
-      
+
     case 1:
-      show('fav', FavNr);
+      show('fav', FavNr, false);
       setCurrentCategory('fav');
       oki();
       break;
-    
+
     case 2:
-      show('dyr', DyrNr);
+      show('dyr', DyrNr, false);
       setCurrentCategory('dyr');
       oki();
       break;
-      
+
     case 3:
-      show('por', PorNr);
+      show('por', PorNr, false);
       setCurrentCategory('por');
       oki();
       break;
-      
+
     case 4:
-      show('lan', LanNr);
+      show('lan', LanNr, false);
       setCurrentCategory('lan');
       oki();
       break;
-      
+
     case 5:
-      show('pla', PlaNr);
+      show('pla', PlaNr, false);
       setCurrentCategory('pla');
       oki();
       break;
-      
+
     case 6:
-      show('div', DivNr);
+      show('div', DivNr, false);
       setCurrentCategory('div');
       oki();
       break;
   }
 }
+
+function showCollections() {
+  clearCollections();
+  removeImages();
+
+  var view = document.getElementById("collections-view");
+
+  // COLLECTIONS ADDED BELOW:
+  // addCollection(view, 'Nyttår 2022-2023', 'Godt Nytt År!', '2023-01-01', 5);
+  addCollection(view, 'Vinter 2023', 'Vi hadde eit mål om å ta nokre vinterbilete medan snøven endå var her. Ganske fornøgd med resultatet!', '2023-02-27', 2);
+}
+
+function clearCollections() {
+  document.getElementById("collections-view").innerHTML = '';
+}
+
+function addCollection(view, heading, subtext, collection_dir, img_nr) {
+  header = document.createElement('h4');
+  header.setAttribute("class", "collection-header");
+  header.innerHTML = heading;
+  view.appendChild(header);
+  text = document.createElement('p');
+  text.setAttribute("class", "collection-text");
+  text.innerHTML = subtext;
+  view.appendChild(text);
+  dir = '../photos/' + collection_dir + '/';
+  console.log(collection_dir);
+  console.log(dir);
+  box = document.createElement('div');
+  box.setAttribute("class", "collection-box");
+  view.appendChild(box);
+  for (let i = 0; i < img_nr; i++) {
+    img_box = document.createElement('div');
+    img_box.setAttribute("class", "photo-box");
+    img = document.createElement('img');
+    img.setAttribute("src", dir + 'img' + pad(i + 1) + ".jpg");
+    img.setAttribute("class", "photo");
+    img.setAttribute("onclick", "imageViewer(src);");
+    img_box.appendChild(img);
+    box.appendChild(img_box);
+  }
+  separator = document.createElement('hr');
+  view.appendChild(separator);
+}
+
 
 function agreeToCookies() {
   localStorage.setItem('hasSeenCookieWarning', 'yes');
@@ -439,7 +501,6 @@ function hideKontaktForm() {
 function oki() {
   document.getElementById("welcome").style.top="-100vh";
   // localStorage.setItem('hasBeenWelcomed', 'yes');
-
 }
 
 function okidoki() {
@@ -459,40 +520,31 @@ function checkWelcome() {
 }
 
 function runCarousel() {
-  carousel = document.getElementsByClassName("carousel");
-  slides = carousel[0].getElementsByClassName("carousel-slide");
+  slides = document.getElementsByClassName("carousel")[0].getElementsByClassName("carousel-slide");
   for (let i = 0; i < slides.length; i++) {
-    console.log(slides[i].className);
     if (slides[i].className.includes(" carousel-slide--1")) {
-      console.log("found carousel-slide--1");
       slides[i].className = slides[i].className.replace(" carousel-slide--1", " carousel-slide-0");
     }
     else if (slides[i].className.includes(" carousel-slide-0")) {
-      console.log("found carousel-slide-0");
       slides[i].className = slides[i].className.replace(" carousel-slide-0", " carousel-slide-1");
     }
     else if (slides[i].className.includes(" carousel-slide-1")) {
-      console.log("found carousel-slide-1");
       slides[i].className = slides[i].className.replace(" carousel-slide-1", " carousel-slide-2");
     }
     else if (slides[i].className.includes(" carousel-slide-2")) {
-      console.log("found carousel-slide-2");
       slides[i].className = slides[i].className.replace(" carousel-slide-2", " carousel-slide-3");
     }
     else if (slides[i].className.includes(" carousel-slide-3")) {
-      console.log("found carousel-slide-3");
       slides[i].className = slides[i].className.replace(" carousel-slide-3", " carousel-slide-4");
     }
     else if (slides[i].className.includes(" carousel-slide-4")) {
-      console.log("found carousel-slide-4");
       slides[i].className = slides[i].className.replace(" carousel-slide-4", " carousel-slide-5");
     }
     else if (slides[i].className.includes(" carousel-slide-5")) {
-      console.log("found carousel-slide-5");
       slides[i].className = slides[i].className.replace(" carousel-slide-5", " carousel-slide--1");
     }
     else {
-      console.log("no match");
+      console.warn("CAROUSEL - no match");
     }
   }
 }
